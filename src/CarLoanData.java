0e337312705c49a56ea5cfce3dcb4b807ad8eb62
sherman210 <sherman7@emich.edu>
2017-04-24 14:27:04 -0400

    This is the one that was supposed to be loaded

---
+++ src/CarLoanData.java
@@ -0,0 +1,68 @@
+package user;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author mochan
+ */
+public class CarLoanData {
+
+    int carprice;
+    int loanlen;
+    double expreturn;
+
+    public int getCarprice() {
+        return carprice;
+    }
+
+    public int getLoanlen() {
+        return loanlen;
+    }
+
+    public double getExpreturn() {
+        return expreturn;
+    }
+
+    public void setCarprice(int carprice) {
+        this.carprice = carprice;
+    }
+
+    public void setLoanlen(int loanlen) {
+        this.loanlen = loanlen;
+    }
+
+    public void setExpreturn(double expreturn) {
+        this.expreturn = expreturn;
+    }
+
+    public double getMonthlyPayment()
+    {
+        return (double)carprice / (double)loanlen;
+        
+    }
+    public String print()
+    {
+        double[] bal = new double[loanlen + 1];
+    
+        String s = "";
+        bal[0] = carprice;
+        for (int i=1; i<loanlen + 1; i++)
+        {
+            bal[i] = bal[i-1] - getMonthlyPayment();
+            bal[i] *= (1 + 8./12.0*0.01);
+            
+            //if (i%12 == 0)
+            {
+                s += "Month " + i + ": $" + String.format("%.2f", bal[i]) + "\n<br>";
+            }
+            
+        }
+        s += "Final Balance: $" + String.format("%.2f", bal[loanlen -1]);
+        return s;
+    }
+}
